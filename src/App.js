af891bba7ca6e1c37d1f9d693579472ee933d0eb
jun1116 <52767291+jun1116@users.noreply.github.com>
2020-10-20 20:19:46 +0900

    props의 재사용예시

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-function Potato({fav}){
-  console.log(fav);
+function Potato(props){
+  console.log(props.fav);
   //console.log(props.fav);
-  return <h1>I like {fav}</h1>;
+  return <h3>I like {props.fav}</h3>;
 }
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
       <Potato fav="kimchi" />
+      <Potato fav="라면" />
+      <Potato fav="삼겹살" />
+      <Potato fav="두루치기" />
+      <Potato fav="김밥" />
       </div>
     );
 }
-
 export default App;
