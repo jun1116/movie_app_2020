8193151fcd36131bd9a6e222da363e663fadb4a3
jun1116 <52767291+jun1116@users.noreply.github.com>
2020-10-20 21:31:26 +0900

    key id 를 부여하는 이유
    리액트 내부적으로 사용하기위해

--- src/App.js
+++ src/App.js
@@ -4,36 +4,39 @@ function Food(props){
   //console.log(props.name);
   return <div>
   <h3>I like {props.name}</h3>
-  <img src={props.img} />
+  <img src={props.img} alt={props.img}/>
   </div>;
 }
 const foodILike = [
   {
+    id:1,
   name: "Kimchi",
   image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
   },
   {
+    id:2,
   name: "bulgogi",
   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTKI8706unb3NS_ATqr7PAccy8aSUcHhNoSOA&usqp=CAU.jpg"
   },
   {
+    id:3,
   name: "samgyetang",
   image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
   }
 ]
 
-function renderFood(dish){
-  //console.log(dish);
-  return <Food name={dish.name} img={dish.image}/>;
-}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
       <div>
-        {console.log(foodILike.map(renderFood))}
-        {foodILike.map(renderFood)}</div>
+        {foodILike.map(dish => (
+          <Food key={dish.id} name={dish.name} img={dish.image}/>
+        )
+        )
+        }
+        </div>
       </div>
     );
 }
